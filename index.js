cb240cadd6d5feaf1c91732042606f48af179b09
purnareddys <33196988+purnareddys@users.noreply.github.com>
2020-06-26 22:07:13 +0530

    Added basic folder structure along with db connection

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+const expressLayouts = require("express-ejs-layouts");
+app.use(express.static("./assets"));
+app.use(expressLayouts);
+
+const db = require("./config/mongoose");
+
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 // use express router
 app.use("/", require("./routes"));
+
+//set up view engine
+app.set("view engine", "ejs");
+app.set("vieusers/profilews", "./views");
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error Occured, ${err}`);
