31cd344de0def832f03876163ac641de63ffc77a
purnareddys <33196988+purnareddys@users.noreply.github.com>
2020-06-29 16:06:12 +0530

    User can post content in the app

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,3 +1,17 @@
-module.exports.post = function (req, res) {
-  res.end("<h1> This is a post</h1>");
+const Post = require("../models/post");
+module.exports.create = function (req, res) {
+  Post.create(
+    {
+      content: req.body.content,
+      user: req.user._id,
+    },
+    function (err, post) {
+      if (err) {
+        console.log("error in creating post");
+        return;
+      }
+
+      return res.redirect("back");
+    }
+  );
 };
